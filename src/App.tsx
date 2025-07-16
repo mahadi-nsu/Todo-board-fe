548ceea274c05be69e3f246886348317fc41caae
mahadi-nsu <mahadi.hassan@northsouth.edu>
2025-07-16 19:51:07 +0600

    feat : Login screen basic design added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import "./App.css";
 import Login from "./features/auth-login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="bg-white rounded-lg shadow-md p-6 min-w-[300px]">
-        <h2 className="m-0 mb-3 text-xl font-semibold">Todo</h2>
-        <p className="text-gray-600">
-          This is a dummy todo card. Your todo will be here!
-        </p>
-      </div>
-      <Login />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/features/auth-login/components/Login.tsx
+++ src/features/auth-login/components/Login.tsx
@@ -1,7 +1,67 @@
 import React from "react";
+import { Form, Input, Button, Card } from "antd";
+
+interface LoginFormValues {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
-  return <div>Login</div>;
+  const onFinish = (values: LoginFormValues) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen w-screen overflow-hidden">
+      <Card className="w-full max-w-md" bordered>
+        <h2 className="text-xl font-semibold text-center mb-6">Login</h2>
+        <Form
+          name="login"
+          layout="vertical"
+          onFinish={onFinish}
+          autoComplete="off"
+          requiredMark={false}
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            required={false}
+            rules={[
+              { required: true, message: "Please input your email!" },
+              { type: "email", message: "Please enter a valid email!" },
+            ]}
+            style={{
+              marginBottom: "12px",
+            }}
+          >
+            <Input placeholder="Enter your email" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password placeholder="Enter your password" />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              Login
+            </Button>
+          </Form.Item>
+
+          <hr className="border-t border-dotted border-neutral-400 my-4" />
+        </Form>
+        <div className="text-center mt-2">
+          <a href="#" className="text-blue-600 hover:underline">
+            Don't registered yet?{" "}
+            <span className="font-semibold">Go to register</span>
+          </a>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
